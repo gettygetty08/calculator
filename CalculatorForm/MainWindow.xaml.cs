906f22b1b4f16557a897aebbc9f9444b1c148175
DESKTOP-2Q58HKQ\getty <gettygetty08@gmail.com>
2020-12-21 23:56:50 +0900

    ・結果表示部分について、画面内に数字が収まるように修正 ・数字ボタン押下時に対応した数字が結果表示部分に表示する ・Cボタン押下時にデフォルト値を表示する ・小数点ボタン押下時に結果表示部分に小数点を含めた数字を表示

--- CalculatorForm/MainWindow.xaml.cs
+++ CalculatorForm/MainWindow.xaml.cs
@@ -20,9 +20,81 @@ namespace CalculatorForm
     /// </summary>
     public partial class MainWindow : Window
     {
+        private resultDisplay _resultDisplay;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            _resultDisplay = new resultDisplay(ResultDisplayLabel);
+            
+
+
         }
+        private void numberButtonClick(object sender,RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            _resultDisplay.showResult(button.Content.ToString());
+        }
+
+        private void decimalPointButtonClick(object sender,RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            _resultDisplay.showDecimalPoint(button.Content.ToString());
+        }
+
+        private void clearButtonClick(object sender,RoutedEventArgs e)
+        {
+            _resultDisplay.clearResult();
+        }
+
+
+
+        private class resultDisplay
+        {
+            private Label _resultDisplayLabel;
+            private static readonly int _maxResultLength = 16;
+            private static readonly string _defaultValue = "0";
+
+                internal resultDisplay(Label label)
+            {
+                _resultDisplayLabel = label;
+            }
+
+            internal void showResult(string inputValue)
+            {
+                string nowResult = getLabelValue();
+
+                if (nowResult.Length > _maxResultLength) return;
+
+                double result = double.Parse($"{(nowResult == _defaultValue ? string.Empty : nowResult)}{inputValue}");
+                //_resultDisplayLabel.Content = string.Format("{0:#,0}", result);
+                _resultDisplayLabel.Content = $"{result:#,0.#########}";
+            }
+
+            internal void showDecimalPoint(string decimalPoint)
+            {
+                string nowResult = getLabelValue();
+                if (nowResult == _defaultValue || nowResult.Contains(decimalPoint)) return;
+
+                _resultDisplayLabel.Content = $"{nowResult}{decimalPoint}";
+
+
+            }
+
+            internal void clearResult()
+            {
+                _resultDisplayLabel.Content = "0";
+            }
+
+            private string getLabelValue()
+            {
+                return _resultDisplayLabel.Content.ToString();
+            }
+
+
+        }
+
+
     }
 }
